733a94187dd25327b8604e1bd781197e874b28a9
Liddall <jlliddal@a483e751b26f.ant.amazon.com>
2019-10-17 16:39:04 +1100

    more event listeners

--- src/App.js
+++ src/App.js
@@ -20,6 +20,30 @@ Auth.configure({ oauth });
 
 const logger = new Logger('AppLog','DEBUG');
 
+const listener = (data) => {
+  logger.error('auth event', data)
+  switch (data.payload.event) {
+  
+      case 'signIn':
+          logger.error('user signed in'); //[ERROR] My-Logger - user signed in
+          break;
+      case 'signUp':
+          logger.error('user signed up');
+          break;
+      case 'signOut':
+          logger.error('user signed out');
+          break;
+      case 'signIn_failure':
+          logger.error('user sign in failed');
+          break;
+      case 'configured':
+          logger.error('the Auth module is configured');
+          
+  }
+}
+
+Hub.listen('auth', listener, 'genericListener');
+
 class App extends Component {
   // setup props
   constructor(props) {
