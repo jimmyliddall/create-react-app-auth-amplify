441525dbccc55ff343754309c7999545b1c39ce4
Liddall <jlliddal@a483e751b26f.ant.amazon.com>
2019-10-17 16:13:15 +1100

    more logging2

--- src/App.js
+++ src/App.js
@@ -26,11 +26,13 @@ class App extends Component {
     super(props);
     this.signOut = this.signOut.bind(this);
     Hub.listen('auth', (data) => {
+        logger.debug('Data', data)
         const { payload } = data;
         logger.debug('A new auth event has happened: ' + data.payload.data.username + ' has ' + data.payload.event);
         this.onAuthEvent(payload);           
         logger.debug('A new auth event has happened: ' + data.payload.data.username + ' has ' + data.payload.event);
-    })
+    });
+    
   }
 
   // define vars in state (could be done in props constructor)
